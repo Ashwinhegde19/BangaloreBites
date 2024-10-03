b04bd3f57d8b07206d6157634ad5d9ecf8f0b3bd
ashwinhegde19 <ashwinhegde19@gmail.com>
2024-10-04 01:05:42 +0530

    add Cart component and update routing

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import ItemList from "./ItemList"
-
+import { useSelector, useDispatch } from 'react-redux';
+import ItemList from './ItemList';
+import { removeFromCart } from '../utils/cartSlice';
 function Cart() {
-    const cartItems = useSelector((store) => store.cart.items)
+  const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+
+ 
 
   return (
     <div className='text-center m-4 p-4'>
-        <h1 className='text-4xl font-bold'>Cart</h1>
-        <p className='text-xl font-semibold'>You have {cartItems.length} items in your cart</p>
-        <div className='flex justify-center'>
-            <button className='px-4 py-2 bg-green-400 text-white font-semibold rounded-lg shadow-md hover:shadow-lg transition-shadow'>Checkout</button>
-            <div>
-                <ItemList items={cartItems} />
-            </div>
+      <h1 className='text-4xl font-bold'>Cart</h1>
+      <p className='text-xl font-semibold'>You have {cartItems.length} items in your cart</p>
+      <div className='flex justify-center'>
+        <div className='6/12 w-auto'>
+          <ItemList items={cartItems}  />
         </div>
-
+      </div>
+     
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,44 +5,46 @@ import { addToCart } from "../utils/cartSlice";
 const ItemList = ({ items }) => {
   const dispatch = useDispatch();
 
-  const handleCart = (items) => {
-    dispatch(addToCart(items));
+  const handleCart = (item) => {
+    dispatch(addToCart(item));
   }
 
   return (
     <div className="p-4">
       {items.map((item) => (
-        <div
-          key={item.card.info.id}
-          className="flex justify-between p-4 mb-6 border border-gray-200 rounded-lg shadow-sm hover:shadow-lg transition-shadow bg-white"
-        >
-          {/* Left section for text */}
-          <div className="flex-1">
-            <h3 className="text-xl font-bold text-gray-800">{item.card.info.name}</h3>
-            
-            <p className="text-green-600 text-lg font-semibold mt-2">
-              ₹{item.card.info.defaultPrice / 100 || item.card.info.finalPrice / 100 || item.card.info.price / 100}
-            </p>
-            <p className="text-gray-600 text-sm mt-2">{item.card.info.description}</p>
-          </div>
+        item.card && item.card.info ? (
+          <div
+            key={item.card.info.id}
+            className="flex justify-between p-4 mb-6 border border-gray-200 rounded-lg shadow-sm hover:shadow-lg transition-shadow bg-white"
+          >
+            {/* Left section for text */}
+            <div className="flex-1">
+              <h3 className="text-xl font-bold text-gray-800">{item.card.info.name}</h3>
+              
+              <p className="text-green-600 text-lg font-semibold mt-2">
+                ₹{item.card.info.defaultPrice / 100 || item.card.info.finalPrice / 100 || item.card.info.price / 100}
+              </p>
+              <p className="text-gray-600 text-sm mt-2">{item.card.info.description}</p>
+            </div>
 
-          {/* Right section for image and button */}
-          <div className="relative w-40 p-2">
-            <img
-              src={CDN_URL + item.card.info.imageId}
-              alt={item.card.info.name}
-              className="w-full h-24 object-cover rounded-lg mb-4"
-            />
-            <div className="absolute bottom-0 left-8">
-              <button className="px-4 py-2 mx-4 bg-orange-500 text-white font-semibold shadow-md rounded-full hover:bg-orange-600 transition-all
-              "
-              onClick={() => {handleCart(items)}}
-              >
-                Add +
-              </button>
+            {/* Right section for image and button */}
+            <div className="relative w-40 p-2">
+              <img
+                src={CDN_URL + item.card.info.imageId}
+                alt={item.card.info.name}
+                className="w-full h-24 object-cover rounded-lg mb-4"
+              />
+              <div className="absolute bottom-0 left-8">
+                <button
+                  className="px-4 py-2 mx-4 bg-orange-500 text-white font-semibold shadow-md rounded-full hover:bg-orange-600 transition-all"
+                  onClick={() => handleCart(item)}
+                >
+                  Add +
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        ) : null
       ))}
     </div>
   );
